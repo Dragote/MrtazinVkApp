4884494e7e1141622561805f0a235d8afb81074d
Dragote <ilem1407@gmail.com>
2019-05-27 21:15:54 +0700

    Подключен Logger

--- app/src/main/java/com/murtazin/vkapp/App.kt
+++ app/src/main/java/com/murtazin/vkapp/App.kt
@@ -4,12 +4,18 @@ package com.murtazin.vkapp
 import com.murtazin.vkapp.dagger.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber.DebugTree
+import timber.log.Timber
 
 
 class App : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG){
+            Timber.plant(DebugTree())
+        }
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
